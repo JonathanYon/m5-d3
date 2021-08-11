cf5bde448ffe33a2d56067e004d4669cc019ad2f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-11 22:04:06 +0200

    port 3003

--- src/app.js
+++ src/app.js
@@ -8,17 +8,17 @@ import { forbidenHandler } from "./errorHandler.js";
 import { genericErrorHandler } from "./errorHandler.js";
 
 const server = express();
-const port = 3000;
+const port = 3003;
 
 server.use(cors());
 server.use(express.json());
 
 server.use("/blogposts", blogPostsRouter);
 
-// server.use(notFoundHandler);
-// server.use(badRequestHandler);
-// server.use(forbidenHandler);
-// server.use(genericErrorHandler);
+server.use(notFoundHandler);
+server.use(badRequestHandler);
+server.use(forbidenHandler);
+server.use(genericErrorHandler);
 
 server.use((req, res) => {
   if (!req.route) {
