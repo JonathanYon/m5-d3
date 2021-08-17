c001af794737cf79263e0b798e1740f409972eeb
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-18 00:19:14 +0200

    .

--- src/services/lib/tool.js
+++ src/services/lib/tool.js
@@ -6,7 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 const cloudinaryStorage = new CloudinaryStorage({
-  cloudinary: process.env.CLOUDINARY_URL, //coming from env
+  cloudinary, //coming from env
   params: {
     folder: "blog Photos",
   },
