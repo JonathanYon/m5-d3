9dcfb85cec7be450b77b53dc057cc07afa7f2ef5
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-18 18:47:27 +0200

    changed id

--- src/services/lib/pdf.js
+++ src/services/lib/pdf.js
@@ -1,33 +1,38 @@
 import PdfPrinter from "pdfmake";
 
 // Define font files
+
 const fonts = {
   Roboto: {
-    normal: "Verdana",
-    bold: "Verdana-Bold",
-    italics: "Verdana-Italic",
-    bolditalics: "Verdana-MediumItalic",
+    normal: "Helvetica",
+    bold: "Helvetica-Bold",
+    italics: "Helvetica-Oblique",
+    bolditalics: "Helvetica-BoldOblique",
   },
 };
 
 //   const PdfPrinter = require('pdfmake');
 export const getPDFStream = (data) => {
-  const printer = new PdfPrinter(fonts);
-  //   const fs = require('fs');
+  try {
+    const printer = new PdfPrinter(fonts);
+    //   const fs = require('fs');
 
-  const docDefinition = {
-    content: [
-      {
-        image: `${data.cover}`,
-      },
-      `${data.category} is the the category`,
-    ],
-  };
-  console.log(docDefinition);
-  const options = {};
+    const docDefinition = {
+      content: [
+        // {
+        //   image: `${data.avatar}`,
+        // },
+        `${data.category} is the the category`,
+      ],
+    };
+    console.log(docDefinition);
+    const options = {};
 
-  const pdfReadStream = printer.createPdfKitDocument(docDefinition, options);
-  //   pdfReadStream.pipe(fs.createWriteStream('document.pdf'));
-  pdfReadStream.end();
-  return pdfReadStream;
+    const pdfReadStream = printer.createPdfKitDocument(docDefinition, options);
+    //   pdfReadStream.pipe(fs.createWriteStream('document.pdf'));
+    pdfReadStream.end();
+    return pdfReadStream;
+  } catch (error) {
+    console.log(error);
+  }
 };
