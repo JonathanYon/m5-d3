7f7c45ff876e7afce866042ba33105458993d9dd
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-19 17:05:10 +0200

    ..

--- src/services/lib/email.js
+++ src/services/lib/email.js
@@ -6,7 +6,7 @@ export const sendEmail = async (recipient) => {
   const msg = {
     to: "yniyoo@gmail.com", // Change to your recipient
     from: "adhanamengisteab@gmail.com", // Change to your verified sender
-    subject: "Test for Email API endpoint",
+    subject: "Test for Email API endpoint second text",
     //   text: 'and easy to do anywhere, even with Node.js',
     html: "<strong>Hello from somewhere and this is a test!.</strong>",
   };
